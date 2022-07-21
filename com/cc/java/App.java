dbae1e0701aac35819cbc5055e575629005816e5
Cing73 <craig@vancleaf>
2022-07-21 15:13:41 +0200

    Java

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,12 +4,18 @@ public class App {
    
  public static void main(String[] args) {
 
-    Cat cat = new Cat("Grizabela", 29);
-    System.out.println(cat.getName());
-    cat.setName("Grizabella");
-    System.out.println(cat.getName());
-    System.out.println(cat.getAge());
+    Cat cat = new Cat("Grizabella", 29);
+    output(cat.getName());
+    output(String.valueOf(cat.getAge()));
+
+    output("Name: " + cat.getName());
+    output("Alter: " + cat.getAge());
     
+   //  System.out.println(cat.getName());
+   //  System.out.println(cat.getAge());
+    
+   // output(inputStr: "hi");
+
     System.out.println("------------------");
 
     Cat cat1 = new Cat("Alonzo", 35);
@@ -17,5 +23,12 @@ public class App {
     System.out.println(cat1.getAge());
 
  }
+
+private static void output(String inputStr) {
+   System.out.println(inputStr);
+}
+// private static void output(int inputNum) {
+//    System.out.println(inputNum);
+// }
 
 }
